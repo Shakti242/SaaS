df32e6260cf08a12bfe14cc8796da63d63c08b37
Shakti Patel <shaktipatel@Shaktis-MacBook-Pro.local>
2024-06-12 18:06:36 -0400

    Settings Page

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,16 @@
 import { Props } from "next/script";
 import React from "react";
+import Sidebar from '@/components/sidebar'
+import InfoBar from "@/components/infobar";
 type props = { children: React.ReactNode }
 const Layout = (props: Props) => {
     return(
         <div className="flex overflow-hidden h-screen">
-            <div className="e-full"> {props.children}
+            <Sidebar />
+            <div className="e-full">
+                <InfoBar/>
+
+                {props.children}
             </div>
         </div>
     )
